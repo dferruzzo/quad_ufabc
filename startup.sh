8728a10b639ec7438ecad424f3d3af7cdafdb647
Diego Ferruzzo <dferruzzo@gmail.com>
2024-02-13 21:53:11 +0000

    Devcontainer configuration added

--- startup.sh
+++ startup.sh
@@ -7,3 +7,6 @@ cd /home/catkin_ws
 catkin_make
 echo "source /home/catkin_ws/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
+cd ~/gzweb/http/client/assets/
+ln -s /home/catkin_ws/src/quad_ufabc/ quad_ufabc
+roscd quad_ufabc
