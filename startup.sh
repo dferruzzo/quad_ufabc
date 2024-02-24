3dc2d705d749d6facf9c2d3b544cbbf1cbf57708
dferruzzo <dferruzzo@gmail.com>
2024-02-24 11:27:06 -0300

    updated devcontainer

--- startup.sh
+++ startup.sh
@@ -2,11 +2,12 @@
 # run this file at start up
 # bash startup.sh
 # neet to restart the terminal after running this file.
+pip install --no-cache-dir -r requirements.txt
 source /opt/ros/noetic/setup.bash
 cd /home/catkin_ws
 catkin_make
 echo "source /home/catkin_ws/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-cd ~/gzweb/http/client/assets/
-ln -s /home/catkin_ws/src/quad_ufabc/ quad_ufabc
+#cd ~/gzweb/http/client/assets/
+#ln -s /home/catkin_ws/src/quad_ufabc/ quad_ufabc
 roscd quad_ufabc
