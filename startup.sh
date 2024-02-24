02110b1882f70fa4ae50d7a1dabcfbd72922c92d
dferruzzo <dferruzzo@gmail.com>
2024-02-24 19:39:25 -0300

    startup.sh update

--- startup.sh
+++ startup.sh
@@ -8,5 +8,5 @@ catkin_make
 echo "source /home/catkin_ws/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 cd ~/gzweb/http/client/assets/
-ln -s /home/catkin_ws/src/quad_ufabc/ quad_ufabc
+#ln -s /home/catkin_ws/src/quad_ufabc/ quad_ufabc
 roscd quad_ufabc
