e080a0659e533d2dffd88d607c102371b3c54d45
root <root@Casa>
2024-03-15 19:33:59 -0300

    startup.sh update

--- startup.sh
+++ startup.sh
@@ -6,7 +6,11 @@ source /opt/ros/noetic/setup.bash
 cd /home/catkin_ws
 catkin_make
 echo "source /home/catkin_ws/devel/setup.bash" >> ~/.bashrc
+
+source /usr/lib/git-core/git-sh-prompt
+echo "PS1='${debian_chroot:+($debian_chroot)} \[\033[36m\]\u\[\033[37m\]@\[\033[33m\]\h: \[\033[32m\]\w\[\033[31;1m\]$(__git_ps1 " (%s)")\[\033[00m\]\$ '" >> ~/.bashrc
+
 source ~/.bashrc
-cd ~/gzweb/http/client/assets/
+#cd ~/gzweb/http/client/assets/
 #ln -s /home/catkin_ws/src/quad_ufabc/ quad_ufabc
 roscd quad_ufabc
